d656f1e40198651af7b950b0faf90b5c471f0e8a
Daniel Murphy <daniel@devasta.ie>
2021-12-06 00:25:05 +0000

    continuing move to Jsonschema...this time more work for members

--- katsuserver/blueprints/members/schemas.py
+++ katsuserver/blueprints/members/schemas.py
@@ -46,3 +46,94 @@ class membersearchschema:
             self.errors = [{error.path[0]: error.message} for error in sorted(v.iter_errors(self.data), key=str)]
             return False
 
+
+
+class memberdetailsschema:
+
+    def __init__(self, data):
+        self.schema = {
+            'type': 'object',
+            'properties': {
+                'title': {
+                    'type': 'string',
+                    'required': False,
+                    "enum": ['Mr.', 'Mrs.', 'Miss', 'Ms.', 'Dr.', 'Prof.']
+                },
+                'forename': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 35
+                },
+                'surname': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 35
+                },
+                'companyname': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 100
+                },
+                'addressline1': {
+                    'type': 'string',
+                    'required': True,
+                    'minLength': 1,
+                    'maxLength': 50
+                },
+                'addressline2': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 50
+                },
+                'city': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 50
+                },
+                'county': {
+                    'type': 'string',
+                    'required': True,
+                    'maxLength': 50
+                },
+                'country': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 50
+                },
+                'postcode': {
+                    'type': 'string',
+                    'required': True,
+                    'pattern': '^[A-Za-z0-9]{3}[ ]{1}[A-Za-z0-9]{4}$'
+                },
+                'homephone': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 20
+                },
+                'mobilephone': {
+                    'type': 'string',
+                    'required': False,
+                    'maxLength': 20
+                },
+                'dateofbiorth': {
+                    'type': 'string',
+                    'required': False,
+                    'format': 'date'
+                }
+            }
+        }
+        self.data = data
+        self.errors = []
+
+    def validate(self):
+        self.errors = []
+        v = jsonschema.Draft202012Validator(self.schema)
+
+        if v.is_valid(self.data):
+            return True
+        else:
+            self.errors = [{error.path[0]: error.message} for error in sorted(v.iter_errors(self.data), key=str)]
+            return False
+
+
+
--- katsuserver/blueprints/members/views.py
+++ katsuserver/blueprints/members/views.py
@@ -1,11 +1,10 @@
 import flask
 import flask_login
 from . import members
-from . import forms
+from . import schemas
 from . import models
 
 from ...models import requires_permission, get_config
-import psycopg2
 
 
 @members.route('/', methods=['GET'])
@@ -16,7 +15,7 @@ def membersearch():
         Allows the user to search for members. Note: We might externalise such an API call in future so it checks
         if the user has a memberid, and if it does overrides search criteria to only return that member ID.
     """
-    form = forms.membersearchform(formdata=flask.request.args)
+    schema = schemas.membersearchschema(data=flask.request.args)
 
     # If the user does not provide a pagenumber or provides something that is not an integer, we just set to 1.
     try:
@@ -28,20 +27,18 @@ def membersearch():
         page = 1
 
     limit = flask.request.args.get('limit') or get_config('PAGINATION_COUNT')['configvalue']
-    if form.validate():
+    if schema.validate():
         try:
             if flask_login.current_user.memberid:
                 memberid = flask_login.current_user.memberid
-            elif form.memberid.data == '':
-                memberid = None
             else:
-                memberid = form.memberid.data
+                memberid = flask.request.args.get('memberid')
 
             memberslist = models.member_get(memberid=memberid,
-                                            surname=None if form.surname.data == '' else form.surname.data,
-                                            addressline1=None if form.addressline1.data == '' else form.addressline1.data,
-                                            county=None if form.county.data == '' else form.county.data,
-                                            postcode=None if form.postcode.data == '' else form.postcode.data,
+                                            surname=flask.request.args.get('surname'),
+                                            addressline1=flask.request.args.get('addressline1'),
+                                            county=flask.request.args.get('county'),
+                                            postcode=flask.request.args.get('postcode'),
                                             offset=((page - 1)*int(limit)),
                                             limit=int(limit)
                                             )
@@ -51,7 +48,7 @@ def membersearch():
         except Exception as e:
             flask.abort(500, e)
     else:
-        flask.abort(400, form.errors)
+        flask.abort(400, schema.errors)
 
 
 @members.route('/<int:memberid>/', methods=['GET'])
@@ -80,32 +77,31 @@ def member_main(memberid):
 @requires_permission('members_create')
 def new_member_entry():
 
-    form = forms.memberdetailsform.from_json(flask.request.json)
+    schema = schemas.memberdetailsschema(data=flask.request.json)
 
-    if form.validate():
+    if schema.validate():
         try:
             newid = models.member_create(entryuserid=flask_login.current_user.userid,
-                                         title=form.title.data,
-                                         forename=form.forename.data,
-                                         surname=form.surname.data,
-                                         companyname=form.companyname.data,
-                                         addressline1=form.addressline1.data,
-                                         addressline2=form.addressline2.data,
-                                         city=form.city.data,
-                                         county=form.county.data,
-                                         country=form.country.data,
-                                         postcode=form.postcode.data,
-                                         homephone=form.homephone.data,
-                                         mobilephone=form.mobilephone.data,
-                                         dateofbirth=form.dateofbirth.data,
-                                         gender=form.gender.data
+                                         title=flask.request.json.get('title'),
+                                         forename=flask.request.json.get('forename'),
+                                         surname=flask.request.json.get('surname'),
+                                         companyname=flask.request.json.get('companyname'),
+                                         addressline1=flask.request.json.get('addressline1'),
+                                         addressline2=flask.request.json.get('addressline2'),
+                                         city=flask.request.json.get('city'),
+                                         county=flask.request.json.get('county'),
+                                         country=flask.request.json.get('country'),
+                                         postcode=flask.request.json.get('postcode'),
+                                         homephone=flask.request.json.get('homephone'),
+                                         mobilephone=flask.request.json.get('mobilephone'),
+                                         dateofbirth=flask.request.json.get('dateofbirth')
                                          )
 
             return flask.jsonify({'memberid': newid}), 201
         except Exception as e:
             flask.abort(500, e)
     else:
-        flask.abort(400, form.errors)
+        flask.abort(400, schema.errors)
 
 
 @members.route('/<int:memberid>/', methods=['PUT'])
@@ -113,25 +109,23 @@ def new_member_entry():
 @requires_permission('members_update')
 def member_update(memberid):
 
-    form = forms.memberdetailsform.from_json(flask.request.json)
+    schema = schemas.memberdetailsschema(data=flask.request.json)
 
-    if form.validate():
+    if schema.validate():
         try:
-            models.member_update(memberid=memberid,
-                                 title=form.title.data,
-                                 forename=form.forename.data,
-                                 surname=form.surname.data,
-                                 companyname=form.companyname.data,
-                                 addressline1=form.addressline1.data,
-                                 addressline2=form.addressline2.data,
-                                 city=form.city.data,
-                                 county=form.county.data,
-                                 country=form.country.data,
-                                 postcode=form.postcode.data,
-                                 homephone=form.homephone.data,
-                                 mobilephone=form.mobilephone.data,
-                                 dateofbirth=form.dateofbirth.data,
-                                 gender=form.gender.data
+            models.member_update(title=flask.request.json.get('title'),
+                                 forename=flask.request.json.get('forename'),
+                                 surname=flask.request.json.get('surname'),
+                                 companyname=flask.request.json.get('companyname'),
+                                 addressline1=flask.request.json.get('addressline1'),
+                                 addressline2=flask.request.json.get('addressline2'),
+                                 city=flask.request.json.get('city'),
+                                 county=flask.request.json.get('county'),
+                                 country=flask.request.json.get('country'),
+                                 postcode=flask.request.json.get('postcode'),
+                                 homephone=flask.request.json.get('homephone'),
+                                 mobilephone=flask.request.json.get('mobilephone'),
+                                 dateofbirth=flask.request.json.get('dateofbirth')
                                  )
             return '', 204
         except KeyError as e:
@@ -139,4 +133,4 @@ def member_update(memberid):
         except Exception as e:
             flask.abort(500, e)
     else:
-        flask.abort(400, form.errors)
+        flask.abort(400, schema.errors)
